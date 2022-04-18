6636d40ec56114bf40f313fe41553421147c5077
edizeqiri <89740646+edizeqiri@users.noreply.github.com>
2022-04-18 23:03:53 +0200

    add parsing

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 
 
 void main_loop();
+char *read_input(void);
 
 int main(int argc, char **argv)
 {
@@ -38,4 +39,43 @@ void main_loop(void)
     free(line);
     free(args);
   } while (status);
+}
+
+char *read_input(void)
+{
+  int size = BUFFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * size);
+
+  // Needs to be an int to check for EOF
+  int character;
+
+  if (!buffer) {
+    fprintf(stderr, "unishell: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    character = getchar();
+
+    // If we hit EOF (End of File, like ctrl+D), replace it with a null character and return.
+    if (character == EOF || character == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = character;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= size) {
+      size += BUFFSIZE;
+      buffer = realloc(buffer, size);
+      if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 }
