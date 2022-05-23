08b70a0fb09c44e6fc1bfe5a4c749170c9d5c4b6
edizeqiri <89740646+edizeqiri@users.noreply.github.com>
2022-05-23 21:16:54 +0200

    fix compile errors

--- main.c
+++ main.c
@@ -8,48 +8,27 @@
 #define TOKEN_BUFFSIZE 64
 #define TOKEN_DELIMS " \t\r\n\a"
 
-int main(int argc, char **argv);
-void main_loop();
-char *read_input();
-void error_exiter(char *trigger);
-char **split_input(char *line);
-int execute(char **args);
+int us_cd(char **args);
+int us_help(char **args);
+int us_exit(char **args);
 
-int main(int argc, char **argv)
-{
-  // Load config files, if any.
+char *intern_strings[] = {
+  "cd",
+  "help",
+  "exit"
+};
 
-  // Run command loop.
-  main_loop();
+int (*intern_func[]) (char **) = {
+  &us_cd,
+  &us_help,
+  &us_exit
+};
 
-  // Perform any shutdown/cleanup.
-
-  return EXIT_SUCCESS;
+int size_intern_strings() {
+  return sizeof(intern_strings) / sizeof(char *);
 }
 
-/**
- * @brief Main loop of the shell which does 3 things.
- * 1. Get input and store it into a string
- * 2. Parse the input
- * 3. Execute the input 
- */
-void main_loop()
-{
-  char *line;
-  char **args;
-  int status;
 
-  // While status is good (0) read-split into args-execute
-  do {
-    printf("=> ");
-    line = read_input();
-    args = split_input(line);
-    status = execute(args);
-
-    free(line);
-    free(args);
-  } while (status);
-}
 
 /**
  * @brief Store input into a buffer which then gets saved into a String
@@ -79,7 +58,7 @@ char *read_input()
   return line;
 }
 
-// helper function
+// us_helper function
 void error_exiter(char *trigger) {
 if (!trigger) {
     fprintf(stderr, "What did you do?!\n");
@@ -133,27 +112,116 @@ char **split_input(char *line) {
  * @param args string array from split_input
  * @return int if failure or success
  */
-int execute(char **args)
-{
-  pid_t pid, wpid;
+int execute(char **args) {
+  pid_t pid, wait_pid;
   int status;
 
   pid = fork();
+
+  // if child
   if (pid == 0) {
-    // Child process
+
+    // v for vector and p to let OS find program and not give full path
     if (execvp(args[0], args) == -1) {
-      perror("lsh");
+      perror("Unishell");
     }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
     // Error forking
-    perror("lsh");
+    perror("Unishell");
   } else {
-    // Parent process
+    
+    // if parent
     do {
-      wpid = waitpid(pid, &status, WUNTRACED);
+      // wait for child to die
+      wait_pid = waitpid(pid, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
 
   return 1;
+}
+
+/**
+ * @brief change directory
+ * 
+ * @param args 
+ * @return int 
+ */
+int us_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "Unishell: expected argument to \"us_cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("Unishell");
+    }
+  }
+  return 1;
+}
+
+/**
+ * @brief exit the shell duh
+ * 
+ * @param args for convinience
+ * @return int 
+ */
+int us_exit(char **args)
+{
+  return 0;
+}
+
+int us_help(char **args)
+{
+  int i;
+  printf("\n|=========================================================================|\n");
+  printf("|                   Unishell from students for students                   |\n");
+  printf("|  Our improved shell will make you feel like learning could never stop!  |\n");
+  printf("|                     The following are built in:                         |\n");
+  printf("|=========================================================================|\n \n");
+
+  for (i = 0; i < size_intern_strings(); i++) {
+    printf("|%d.  %s\n",i+1 , intern_strings[i]);
+  }
+
+  return 1;
+}
+
+
+/**
+ * @brief Main loop of the shell which does 3 things.
+ * 1. Get input and store it into a string
+ * 2. Parse the input
+ * 3. Execute the input 
+ */
+void main_loop()
+{
+  char *line;
+  char **args;
+  int status;
+
+  us_help(NULL);
+  // While status is good (0) read-split into args-execute
+  do {
+    printf("=> ");
+    line = read_input();
+    args = split_input(line);
+    status = execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  main_loop();
+
+
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
 }
