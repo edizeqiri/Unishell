cdf7f11c3c781f818da6cec9c399c8bc0c410f4f
edizeqiri <89740646+edizeqiri@users.noreply.github.com>
2022-05-23 19:22:49 +0200

    add line parser

--- main.c
+++ main.c
@@ -5,9 +5,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFFSIZE 1024
-
-
 void main_loop();
 char *read_input();
 
@@ -51,7 +48,8 @@ char *read_input()
     // Look for EOF (End of File)
     if (feof(stdin)) {
       exit(EXIT_SUCCESS); 
-    } 
+    }
+
     // Something went wrong
     else  
     {
@@ -61,4 +59,45 @@ char *read_input()
   }
 
   return line;
+}
+
+// helper function
+void error_exiter(char *trigger) {
+if (!trigger) {
+    fprintf(stderr, "What did you do?!\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+#define TOKEN_BUFFSIZE 64
+#define TOKEN_DELIMS " \t\r\n\a"
+char **split_input(char *line) {
+
+  int bufsize = TOKEN_BUFFSIZE, index = 0;
+
+  // String array
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  error_exiter(token);
+
+  // Take whole String and partialize it into tokens which will then be processed and accessed by a pointer
+  token = strtok(line, TOKEN_DELIMS);
+
+  // Put tokens into string array one by one
+  while (token != NULL) {
+    tokens[index] = token;
+    index++;
+
+    // If buffer to small double its size and reallocate
+    if (index >= bufsize) {
+      bufsize += TOKEN_BUFFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      error_exiter(token);
+    }
+
+    token = strtok(NULL, TOKEN_DELIMS);
+  }
+  tokens[index] = NULL;
+  return tokens;
 }
